1d88fac71067aeea557fa75b88c07eec15bfc633
sekhar1805 <chandu.shekar4039@gmail.com>
2023-10-29 14:35:32 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
 
-export default App;
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [range, setRange] = useState(1);
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+  function handleReset() {
+    setCount(0);
+    setRange(1);
+  }
+  return (
+    <div
+      style={{
+        margin: "20px auto",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ padding: "15px" }}>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={range}
+          onChange={(e) => setRange(Number(e.target.value))}
+        />
+        <span>Range:{range}</span>
+      </div>
+      <div>
+        <button onClick={() => setCount((c) => c - range)}>-</button>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+        <button onClick={() => setCount((c) => c + range)}>+</button>
+      </div>
+      <p>
+        <span>
+          {count === 0
+            ? "Today is"
+            : count > 0
+            ? `${count} days from Today is `
+            : `${Math.abs(count)} days ago was`}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+      {count !== 0 || range !== 1 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : null}
+    </div>
+  );
+}
